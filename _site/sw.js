64befcdc88218d53999fbfbc69f4b60b76295e80
fomid8 <fati.omid2000@gmail.com>
2023-12-31 13:20:22 +0330

    update config

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704015917';
+const cacheName = 'chirpy-1704016195';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
