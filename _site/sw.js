1ad26802d9529c8b665d651e14a69c81459adbb1
fomid8 <fati.omid2000@gmail.com>
2024-01-15 15:23:05 +0330

    reverting to f691545

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1705317798';
+const cacheName = 'chirpy-1705319555';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
