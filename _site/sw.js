07fb6222b446be4d1c59f71d891b0068bed3bd47
fomid8 <fati.omid2000@gmail.com>
2024-01-28 20:30:09 +0330

    add tars post

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706117297';
+const cacheName = 'chirpy-1706117799';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
