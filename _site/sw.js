41373a8249f612a2e6b52d404596d67a10c4d197
fomid8 <fati.omid2000@gmail.com>
2024-02-27 14:39:13 +0330

    edit ghol post3

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1708971233';
+const cacheName = 'chirpy-1709032110';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
--- _tabs/about.md
+++ _tabs/about.md
@@ -4,5 +4,5 @@ icon: fas fa-info-circle
 order: 4
 ---
 
-> :)
+> وب سایت شخصی من :)
 {: .prompt-tip }
